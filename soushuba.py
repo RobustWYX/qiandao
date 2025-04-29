1024fa305dca0150100feea3ee1359b3411e2649
RobustWYX <124418980+RobustWYX@users.noreply.github.com>
2025-04-29 13:52:10 +0800

    Update soushuba.py

--- soushuba.py
+++ soushuba.py
@@ -67,7 +67,7 @@ class SouShuBaClient:
         self.answer = answer
         self._common_headers = {
             "Host": f"{ hostname }",
-            "Connection": "keep-alive",
+            "Connection": "close",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
             "Accept-Language": "zh-CN,cn;q=0.9",
